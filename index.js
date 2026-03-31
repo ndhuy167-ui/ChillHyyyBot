51ad779176224d6e04ddb446950f9dde5c614ccd
ndhuy167-ui <ndhuy167@gmail.com>
2026-03-31 11:40:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 import './src/app.js';
 
+const { joinVoiceChannel } = require('@discordjs/voice');
+
+client.once('ready', async () => {
+    console.log(`Bot online: ${client.user.tag}`);
+
+    const channel = client.channels.cache.get("ID_ROOM_VOICE");
+
+    if (channel) {
+        joinVoiceChannel({
+            channelId: channel.id,
+            guildId: channel.guild.id,
+            adapterCreator: channel.guild.voiceAdapterCreator,
+        });
+
+        console.log("Đã vào voice 🔥");
+    } else {
+        console.log("Không tìm thấy room");
+    }
+});
